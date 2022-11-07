a1641fe25e92848c4aea7e7d9a806e91e18a5d82
Nicholas Owen Weber <noweber@gmail.com>
2022-11-06 18:52:06 -0800

    Level Generation Scale is Adjusted for Smaller Data Tiles

--- DGMD E-14 Final Project/Assets/Source/Scripts/LevelController.cs
+++ DGMD E-14 Final Project/Assets/Source/Scripts/LevelController.cs
@@ -2,19 +2,22 @@ using Assets.Source;
 using System;
 using UnityEngine;
 
-public class LevelGeneration : MonoBehaviour
+public class LevelController : MonoBehaviour
 {
+    private bool isLevelGenerated;
+
     // Properties related the floor of the level
     public Transform FloorTransform = null;
     public Transform DangerFloorTransform = null;
-    private bool floorIsGenerated = false;
+    private bool floorIsGenerated;
 
     // TODO: Ceiling and walls
-    public decimal MinLevelWidth = 1.0m;
-    public decimal MaxLevelWidth = 4.0m;
-    public decimal MinLevelLength = 2.0m;
-    public decimal MaxLevelLength = 4.0m;
+    public decimal MinLevelWidth = 4.0m;
+    public decimal MaxLevelWidth = 16.0m;
+    public decimal MinLevelLength = 8.0m;
+    public decimal MaxLevelLength = 24.0m;
     private Tuple<int, int> levelDimensions;
+    private float levelSizeScaleFactor = 0.25f;
     private int[,] levelData;
 
     // Start is called before the first frame update
@@ -35,12 +38,32 @@ public class LevelGeneration : MonoBehaviour
 
     void FixedUpdate()
     {
-        if(!floorIsGenerated && FloorTransform != null)
+        if(!isLevelGenerated)
+        {
+            GenerateLevel();
+        }
+    }
+
+    public void RegenerateLevel()
+    {
+        isLevelGenerated = false;
+        floorIsGenerated = false;
+    }
+
+    private void GenerateLevel()
+    {
+        if (!floorIsGenerated && FloorTransform != null)
         {
             // TODO: Generate the floor with tiles
             // Change Floor Size:
-            FloorTransform.localScale = new Vector3(levelDimensions.Item1, 1.0f, levelDimensions.Item2);
-            DangerFloorTransform.localScale = new Vector3(levelDimensions.Item1 * 2.0f, 1.0f, levelDimensions.Item2 * 2.0f);
+            float levelFloorWidth = levelDimensions.Item1 * levelSizeScaleFactor;
+            float levelFloorHeight = levelDimensions.Item2 * levelSizeScaleFactor;
+            FloorTransform.localScale = new Vector3(levelFloorWidth, 1.0f, levelFloorHeight);
+            DangerFloorTransform.localScale = new Vector3(levelFloorWidth * 2.0f, 1.0f, levelFloorHeight * 2.0f);
+            floorIsGenerated = true;
         }
+
+        isLevelGenerated = true;
     }
 }
+
