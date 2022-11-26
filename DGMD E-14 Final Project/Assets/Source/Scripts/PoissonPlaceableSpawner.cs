e5bbc01ade01e964758b15bea71e7baa7e16682c
Nicholas Owen Weber <noweber@gmail.com>
2022-11-25 18:13:53 -0800

    Essential vs Non-Essential Spawns

--- DGMD E-14 Final Project/Assets/Source/Scripts/PoissonPlaceableSpawner.cs
+++ DGMD E-14 Final Project/Assets/Source/Scripts/PoissonPlaceableSpawner.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 
 public class PoissonPlaceableSpawner : MonoBehaviour
 {
-    public List<GameObject> ObjectsToSpawn;
-    public Vector3 SpawnPlaneOffset = new(0, 0, 0);
-    public Vector2Int XZSpawnPlaneSize = new(-1, -1);
-    private int spawnIndex = 0;
-    public int CandiatePointAttempts = 100;
+    public List<GameObject> EssentialObjectsToSpawn;
+    public int HowManyOfEachEssentialObjectToSpawn = 1;
+    public List<GameObject> NonEssentialObjectsToSpawn;
+    public Vector3 SpawnPlaneOffset = new Vector3(0, 0, 0);
+    public Vector2Int XZSpawnPlaneSize = new Vector2Int(-1, -1);
     private int[,] levelData;
-    private List<Vector2> spawnPoints = new();
-
     private bool levelIsFull;
     private bool spawningIsReady;
+
+    // TODO: Spawn plane tiles one time only if flagged to make a plane.
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,13 +26,13 @@ public class PoissonPlaceableSpawner : MonoBehaviour
         else
         {
             levelData = new int[(int)XZSpawnPlaneSize.x, (int)XZSpawnPlaneSize.y];
-            BlockLevelMiddleFromSpawns();
+            BlockCenterOfPlaneFromSpawns();
             spawningIsReady = true;
         }
 
-        if (ObjectsToSpawn != null)
+        if (EssentialObjectsToSpawn != null)
         {
-            Debug.Log("ObjectsToSpawn count: " + ObjectsToSpawn.Count);
+            Debug.Log("ObjectsToSpawn count: " + EssentialObjectsToSpawn.Count);
         }
     }
 
@@ -42,10 +43,11 @@ public class PoissonPlaceableSpawner : MonoBehaviour
         Debug.Log("Spawn plane size set: (" + XZSpawnPlaneSize.x + ", " + XZSpawnPlaneSize.y + ")");
         levelData = new int[XZSpawnPlaneSize.x, XZSpawnPlaneSize.y];
         spawningIsReady = true;
-        BlockLevelMiddleFromSpawns();
+        BlockCenterOfPlaneFromSpawns();
     }
 
-    private void BlockLevelMiddleFromSpawns()
+    public int RadiusOfUnspawnableCenter = 3;
+    private void BlockCenterOfPlaneFromSpawns()
     {
         if (levelData == null)
         {
@@ -58,14 +60,13 @@ public class PoissonPlaceableSpawner : MonoBehaviour
             return;
         }
 
-
         int centerX = XZSpawnPlaneSize.x / 2;
         int centerZ = XZSpawnPlaneSize.y / 2;
         Debug.Log("Blocking center of level from spawns at: (" + centerX + ", " + centerZ + ").");
         levelData[centerX, centerZ] = 1;
-        for (int x = centerX - 2; x < centerX + 2; x++)
+        for (int x = centerX - RadiusOfUnspawnableCenter; x < centerX + RadiusOfUnspawnableCenter; x++)
         {
-            for (int z = centerZ - 2; z < centerZ + 2; z++)
+            for (int z = centerZ - RadiusOfUnspawnableCenter; z < centerZ + RadiusOfUnspawnableCenter; z++)
             {
                 if (x >= 0 && z >= 0 && x < XZSpawnPlaneSize.x && z < XZSpawnPlaneSize.y)
                 {
@@ -75,6 +76,7 @@ public class PoissonPlaceableSpawner : MonoBehaviour
         }
     }
 
+    int essentialSpawnIndex = 0;
     // Update is called once per frame
     void Update()
     {
@@ -94,39 +96,58 @@ public class PoissonPlaceableSpawner : MonoBehaviour
             return;
         }
 
-        // TODO: Spawn plane tiles one time only if flagged to make a plane.
-
         // Check if enough time has passed between the last spawn:
         if (!IsItTimeToSpawn(Time.deltaTime))
         {
             return;
         }
 
+        // Spawn Essential Objects:
+        while(essentialSpawnIndex < this.EssentialObjectsToSpawn.Count)
+        {
+            for(int i = 0; i < this.HowManyOfEachEssentialObjectToSpawn; i++)
+            {
+                if (!this.TrySpawnPlaceableObject(this.EssentialObjectsToSpawn[essentialSpawnIndex]))
+                {
+                    // Return if another object is unable to be spawned.
+                    Debug.LogWarning("Not all essential placeable objects may have been spawned.");
+                    return;
+                }
+                
+            }
+            essentialSpawnIndex++;
+        }
+
+        // Spawn Non-Essential Objects:
+        bool successfullSpawnedLastObject = true;
+        while (successfullSpawnedLastObject)
+        {
+            int indexOfRandomObjectToSpawn = Random.Range(0, this.NonEssentialObjectsToSpawn.Count - 1);
+            successfullSpawnedLastObject = this.TrySpawnPlaceableObject(this.NonEssentialObjectsToSpawn[indexOfRandomObjectToSpawn]);
+            PrintRemainingPlaneCapacity();
+        }
+
+        /*
         int numberOfTries = 0;
         while (numberOfTries < CandiatePointAttempts)
         {
-            // TODO: Use previous spawn as spawn points and search randomly within their radius:
-
             int xPositionCandidate = RandomNumbers.GetRandomIntegerInclusive(1, XZSpawnPlaneSize.x - 1);
             int yPositionCandidate = RandomNumbers.GetRandomIntegerInclusive(1, XZSpawnPlaneSize.y - 1);
             Debug.Log(levelData);
             if (levelData[xPositionCandidate, yPositionCandidate] == 0)
             {
                 // TODO: Check for any other objects within the radius of this spawn point
-                if (ObjectsToSpawn[spawnIndex] != null)
+                if (EssentialObjectsToSpawn[spawnIndex] != null)
                 {
                     int randomRotation = Random.Range(0, 4) * 90;
-                    Instantiate(ObjectsToSpawn[spawnIndex],
+                    Instantiate(EssentialObjectsToSpawn[spawnIndex],
                         new Vector3(this.gameObject.transform.position.x - (XZSpawnPlaneSize.x / 2) + SpawnPlaneOffset.x + xPositionCandidate,
                         this.gameObject.transform.position.y + SpawnPlaneOffset.y,
                         this.gameObject.transform.position.z - (XZSpawnPlaneSize.y / 2) + SpawnPlaneOffset.z + yPositionCandidate), // Added to Z here because objects are only spawned on the XZ plane.
                         Quaternion.Euler(new Vector3(0, randomRotation, 0)));
                     levelData[xPositionCandidate, yPositionCandidate] = 1;
 
-                    // TODO: SPAWN THE FIRST OBJECT AT THE CENTER OF THE ROOM AND THEN SAMPLE FROM THERE
-                    // todo: poisson disc sampling
-                    // todo: add object as spawn point
-                    int radius = GetPlaceableRadius(ObjectsToSpawn[spawnIndex]);
+                    int radius = GetPlaceableRadius(EssentialObjectsToSpawn[spawnIndex]);
 
                     // Block tiles around the spawn point based on the radius:
                     for (int x = xPositionCandidate - radius; x < xPositionCandidate + radius; x++)
@@ -146,7 +167,6 @@ public class PoissonPlaceableSpawner : MonoBehaviour
             numberOfTries++;
         }
 
-        // TODO: use the spawn points to tell if the level is full when a spawn has failed at every point:
         // If the number of tries to place an object failed, then the level is full:
         if (numberOfTries >= CandiatePointAttempts)
         {
@@ -157,17 +177,86 @@ public class PoissonPlaceableSpawner : MonoBehaviour
         {
             // Increment the spawn index so that the next object spawns next time:
             spawnIndex += 1;
-            if (spawnIndex >= ObjectsToSpawn.Count)
+            if (spawnIndex >= EssentialObjectsToSpawn.Count)
             {
                 spawnIndex = 0;
             }
             PrintRemainingPlaneCapacity();
+        }*/
+    }
+
+    public int SpawnPointCandiateAttempts = 100;
+    private bool TrySpawnPlaceableObject(GameObject placeableObject)
+    {
+        if (placeableObject == null)
+        {
+            Debug.LogError("Placeable object is null and cannot be spawned.");
+            return false;
         }
+
+        if (levelIsFull)
+        {
+            return false;
+        }
+
+        int numberOfTries = 0;
+        while (numberOfTries < SpawnPointCandiateAttempts)
+        {
+            int xPositionCandidate = RandomNumbers.GetRandomIntegerInclusive(1, XZSpawnPlaneSize.x - 1);
+            int yPositionCandidate = RandomNumbers.GetRandomIntegerInclusive(1, XZSpawnPlaneSize.y - 1);
+            if (levelData[xPositionCandidate, yPositionCandidate] == 0)
+            {
+                // TODO: Check for any other objects within the radius of this spawn point
+                int randomRotation = Random.Range(0, 4) * 90;
+                Instantiate(placeableObject,
+                    new Vector3(this.gameObject.transform.position.x - (XZSpawnPlaneSize.x / 2) + SpawnPlaneOffset.x + xPositionCandidate,
+                    this.gameObject.transform.position.y + SpawnPlaneOffset.y,
+                    this.gameObject.transform.position.z - (XZSpawnPlaneSize.y / 2) + SpawnPlaneOffset.z + yPositionCandidate), // Added to Z here because objects are only spawned on the XZ plane.
+                    Quaternion.Euler(new Vector3(0, randomRotation, 0)));
+                levelData[xPositionCandidate, yPositionCandidate] = 1;
+
+                int radius = GetPlaceableRadius(placeableObject);
+
+                // Block tiles around the spawn point based on the radius:
+                for (int x = xPositionCandidate - radius; x < xPositionCandidate + radius; x++)
+                {
+                    for (int z = yPositionCandidate - radius; z < yPositionCandidate + radius; z++)
+                    {
+                        if (x >= 0 && x < XZSpawnPlaneSize.x && z >= 0 && z < XZSpawnPlaneSize.y)
+                        {
+                            levelData[x, z] = radius;
+                        }
+                    }
+                }
+
+                break;
+            }
+            numberOfTries++;
+        }
+
+        // If the number of tries to place an object failed, then the level is full:
+        if (numberOfTries >= SpawnPointCandiateAttempts)
+        {
+            levelIsFull = true;
+            Debug.Log("The spawn plane is full.");
+        }/*
+        else
+        {
+            // Increment the spawn index so that the next object spawns next time:
+            essentialSpawnIndex += 1;
+            if (essentialSpawnIndex >= EssentialObjectsToSpawn.Count)
+            {
+                essentialSpawnIndex = 0;
+            }
+        }*/
+
+        PrintRemainingPlaneCapacity();
+        return true;
     }
 
     private bool IsPlaneReadyToSpawn()
     {
-        if (spawnIndex < ObjectsToSpawn.Count && XZSpawnPlaneSize.x > 0 && XZSpawnPlaneSize.y > 0)
+        if (this.EssentialObjectsToSpawn.Count != 0 && this.NonEssentialObjectsToSpawn.Count != 0 && XZSpawnPlaneSize.x > 0 && XZSpawnPlaneSize.y > 0)
         {
             return true;
         }
@@ -210,11 +299,11 @@ public class PoissonPlaceableSpawner : MonoBehaviour
     private void PrintRemainingPlaneCapacity()
     {
         int emptyCells = 0;
-        for(int i = 0; i < XZSpawnPlaneSize.x; i++)
+        for (int i = 0; i < XZSpawnPlaneSize.x; i++)
         {
             for (int j = 0; j < XZSpawnPlaneSize.y; j++)
             {
-                if(levelData[i, j] == 0)
+                if (levelData[i, j] == 0)
                 {
                     emptyCells++;
                 }
